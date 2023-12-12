f0d40b18b41777ab910aefaffb4b3396e5152a5c
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2023-12-12 14:46:01 +0100

    project ch-java-2023-spring-boot-rick-and-morty-api created and added spring-boot-starter-webflux-Dependency

---
+++ src/main/java/com/example/chjava2023springbootrickandmortyapi/ChJava2023SpringBootRickAndMortyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.chjava2023springbootrickandmortyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChJava2023SpringBootRickAndMortyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChJava2023SpringBootRickAndMortyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chjava2023springbootrickandmortyapi/ChJava2023SpringBootRickAndMortyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chjava2023springbootrickandmortyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChJava2023SpringBootRickAndMortyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
